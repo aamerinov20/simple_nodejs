0bea41dd30cd00b8293787008bcbe2a5b3d61140
aamerinov20 <aamerinov20@gmail.com>
2022-08-09 21:34:53 +0600

    pc commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello People!');
+  res.send('Hell0000 w0rlD!');
 });
 
 app.listen(3000, function () {

ba2b86d4e52c883b49a7e5d7efa09483f87ff410
aamerinov20 <aamerinov20@gmail.com>
2022-08-10 04:20:36 +0600

    push dockerhub

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('new version');
+  res.send('the newest version!!!');
 });
 
 app.listen(3000, function () {
